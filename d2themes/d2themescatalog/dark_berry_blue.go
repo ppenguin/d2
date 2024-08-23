fdbc460957d34762b9b8ff046c2c7d3b767502a3
ppenguin <hieronymusv@gmail.com>
2024-08-23 22:55:56 +0200

    Add new darktheme: Dark berry blue

---
+++ d2themes/d2themescatalog/dark_berry_blue.go
@@ -0,0 +1,25 @@
+package d2themescatalog
+
+import "oss.terrastruct.com/d2/d2themes"
+
+var DarkBerryBlue = d2themes.Theme{
+	ID:   205,
+	Name: "Dark berry blue",
+	Colors: d2themes.ColorPalette{
+		Neutrals: d2themes.DarkNeutral,
+
+		B1: "#E5F3FF",
+		B2: "#BCDDFB",
+		B3: "#77AFE3",
+		B4: "#2353DD",
+		B5: "#0F36A7",
+		B6: "#102566",
+
+		AA2: "#DACEFB",
+		AA4: "#A162D3",
+		AA5: "#5639A5",
+
+		AB4: "#FFDEF1",
+		AB5: "#EA99C6",
+	},
+}
